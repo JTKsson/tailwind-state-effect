f39ba2b002c0143379b7229b17deeab0afd0fd38
Timothy Karlsson <timothy.karlsson@edu.cmeducations.se>
2023-09-29 15:51:08 +0200

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Body from './components/Body'
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+    <Body/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
